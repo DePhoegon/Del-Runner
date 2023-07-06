28068e2c9aa2cae1e4b8d28a3a79036a3c086923
Kerns Noel (Acer) <45953503+DePhoegon@users.noreply.github.com>
2023-07-05 20:34:16 -0600

    Day 6 - Cloudy with a chance of speed

--- Assets/scripts/ManualResetObject.cs
+++ Assets/scripts/ManualResetObject.cs
@@ -8,10 +8,17 @@ public class ManualResetObjects : MonoBehaviour
     public ObjectMover object2Respawn;
     public bool resetObjectWithRNG;
     public bool enableRespawn;
+    public bool respawnOnX;
     public bool enableShiftPos;
-    public float objectOneShiftAmmount;
-    public float objectTwoShiftAmmount;
+    public float objectOneShiftXAmmount;
+    public float objectOneShiftYAmmount;
+    public float objectOneShiftZAmmount;
+    public float objectTwoShiftXAmmount;
+    public float objectTwoShiftYAmmount;
+    public float objectTwoShiftZAmmount;
     public string shiftAxises;
+    public int possibleSpeedIncrease;
+    public bool increaseSpeed;
     public bool restrictX;
     public bool restrictY;
     public bool restrictZ;
@@ -24,14 +31,18 @@ public class ManualResetObjects : MonoBehaviour
     string boundX;
     string boundY;
     string boundZ;
+    private int respawn1Speed;
+    private int respawn2Speed;
 
     private void Start()
     {
         boundX = restrictX ? "x" : "none";
         boundY = restrictY ? "y" : "none";
         boundZ = restrictZ ? "z" : "none";
-        if (!enableShiftPos) { objectOneShiftAmmount = 0; shiftAxises = null; }
+        if (!enableShiftPos) { objectOneShiftXAmmount = 0; shiftAxises = null; }
         if (object2Respawn == null) { object2Respawn = object1Respawn; }
+        respawn1Speed = object1Respawn.GetSpeed();
+        respawn2Speed = object2Respawn.GetSpeed();
     }
     private float adjustedRange(float maxShift, float basePos, string xyz)
     {
@@ -43,30 +54,47 @@ public class ManualResetObjects : MonoBehaviour
     {
         if (object1Respawn.gameObject.tag == other.gameObject.tag || other.gameObject.tag == object2Respawn.gameObject.tag)
         {
+            float oX = other.transform.position.x;
+            float oY = other.transform.position.y;
+            float oZ = other.transform.position.z;
             Destroy(other.gameObject);
             if (enableRespawn)
             {
                 Vector3 objectOnePos = object1Respawn.gameObject.transform.position;
                 Vector3 objectTwoPos = object2Respawn.gameObject.transform.position;
+                if (respawnOnX)
+                {
+                    objectOnePos.Set(oX, objectOnePos.y, objectOnePos.z);
+                    objectTwoPos.Set(oX, objectTwoPos.y, objectTwoPos.z);
+                }
                 if (shiftAxises != null)
                 {
                     
-                    objectOnePos = new Vector3(shiftAxises.Contains("x") ? adjustedRange(objectOneShiftAmmount, objectOnePos.x, boundX) : objectOnePos.x, shiftAxises.Contains("y") ? adjustedRange(objectOneShiftAmmount, objectOnePos.y, boundY) : objectOnePos.y , shiftAxises.Contains("z") ? adjustedRange(objectOneShiftAmmount, objectOnePos.z, boundZ) : objectOnePos.z);
-                    objectTwoPos = new Vector3(shiftAxises.Contains("x") ? adjustedRange(objectTwoShiftAmmount, objectTwoPos.x, boundX) : objectTwoPos.x, shiftAxises.Contains("y") ? adjustedRange(objectTwoShiftAmmount, objectTwoPos.y, boundY) : objectTwoPos.y, shiftAxises.Contains("z") ? adjustedRange(objectTwoShiftAmmount, objectTwoPos.z, boundZ) : objectTwoPos.z);
+                    objectOnePos = new Vector3(shiftAxises.Contains("x") ? adjustedRange(objectOneShiftXAmmount, objectOnePos.x, boundX) : objectOnePos.x, shiftAxises.Contains("y") ? adjustedRange(objectOneShiftYAmmount, objectOnePos.y, boundY) : objectOnePos.y , shiftAxises.Contains("z") ? adjustedRange(objectOneShiftZAmmount, objectOnePos.z, boundZ) : objectOnePos.z);
+                    objectTwoPos = new Vector3(shiftAxises.Contains("x") ? adjustedRange(objectTwoShiftXAmmount, objectTwoPos.x, boundX) : objectTwoPos.x, shiftAxises.Contains("y") ? adjustedRange(objectTwoShiftYAmmount, objectTwoPos.y, boundY) : objectTwoPos.y, shiftAxises.Contains("z") ? adjustedRange(objectTwoShiftZAmmount, objectTwoPos.z, boundZ) : objectTwoPos.z);
                 }
                 if (resetObjectWithRNG)
                 {
                     bool isObjectOne = UnityEngine.Random.Range(0, 20) < 10;
                     ObjectMover respawnMe = resetObjectWithRNG ? isObjectOne ? object1Respawn : object2Respawn : object1Respawn;
                     Vector3 outPos = resetObjectWithRNG ? isObjectOne ? objectOnePos : objectTwoPos : objectOnePos;
-                    respawnMe.RespawnObjects(outPos, respawnMe.transform.rotation);
+                    variableRespawn(respawnMe, outPos, respawnMe.transform.rotation, respawnMe.Equals(object1Respawn));
                 }
-                else
+                else 
                 {
-                    if (object1Respawn.gameObject.tag == other.gameObject.tag) { object1Respawn.RespawnObjects(objectOnePos, object1Respawn.transform.rotation); }
-                    else { object2Respawn.RespawnObjects(objectTwoPos, object2Respawn.transform.rotation); }
+                    if (object1Respawn.gameObject.tag == other.gameObject.tag) { variableRespawn(object1Respawn, objectOnePos, object1Respawn.transform.rotation, true); }
+                    else { variableRespawn(object2Respawn, objectTwoPos, object2Respawn.transform.rotation, false); }
                 }
             }
         }
+    }
+     private void variableRespawn(ObjectMover target, Vector3 postion, Quaternion rotation, bool isObjectOne)
+    {
+        if (increaseSpeed) 
+        {
+            int targetSpeed = isObjectOne ? respawn1Speed : respawn2Speed;
+            int spd = Random.Range(targetSpeed, possibleSpeedIncrease + targetSpeed);
+            target.RespawnObjects(postion, rotation, spd);
+        } else { target.RespawnObjects(postion, rotation); }
     }
 }
--- Assets/scripts/Object Mover.cs
+++ Assets/scripts/Object Mover.cs
@@ -1,19 +1,43 @@
 using UnityEngine;
 
 public class ObjectMover : MonoBehaviour {
-    public float speed;
+    public int speed;
+    public int maxSpeed;
     public GameObject objectPrefab;
     public bool enableRespawn;
+    public bool speedVariance;
+    public int speedVarianceAmmount;
+    private void Start()
+    {
+        if (speedVariance)
+        {
+            int lowerspeed = speedVarianceAmmount / 2;
+            int variedspeed = Random.Range(0, speedVarianceAmmount) - lowerspeed;
+            speed += variedspeed;
+            if (maxSpeed != 0) { maxSpeed += variedspeed; }
+        }
+        if (maxSpeed == 0) { maxSpeed = speed; }
+    }
 
     // Update is called once per frame
     void Update() {
+        if (speed > maxSpeed) { speed = maxSpeed; }
         transform.Translate(Vector3.forward * -speed * Time.deltaTime);
     }
-    public void SetSpeed(float speedIn) { speed = speedIn; }
+    public void SetSpeed(int speedIn) { speed = speedIn; }
+    public int GetSpeed() { return speed; }
 
     private void OnBecameInvisible() 
     {
        
+    }
+    public void RespawnObjects(Vector3 position, Quaternion rotation, int speed)
+    {
+        int spd = objectPrefab.GetComponent<ObjectMover>().GetSpeed();
+        GameObject newtarget = objectPrefab;
+        newtarget.GetComponent<ObjectMover>().SetSpeed(speed);
+        Instantiate(newtarget, position, rotation);
+        objectPrefab.GetComponent<ObjectMover>().SetSpeed(spd);
     }
     public void RespawnObjects(Vector3 position, Quaternion rotation) 
     {
